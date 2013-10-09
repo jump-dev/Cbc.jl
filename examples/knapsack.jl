56f1749d16e3d1f265f6e7e2cd32966f9441c7e5
Miles Lubin <miles.lubin@gmail.com>
2013-10-09 13:03:44 -0400

    update old example

--- examples/knapsack.jl
+++ examples/knapsack.jl
@@ -1,15 +1,16 @@
 
-using CoinMP
+using MathProgBase
 
 function knapsack()
     f = Float64[3, 2, 3, 1, 2]
     A = [7. 2. 9. 3. 1.]
-    types = fill(INTEGER, 5)
     capacity = 10.
-    (z, x, flag) = mixintprog(-f, A, [], [capacity], [], ones(5), types; LogLevel=0)
-    println("Solution status: $flag")
-    println("Optimal value: $z")
-    println("Solution vector: $x") # should be -7
+    solution = mixintprog(-f, A, '<', capacity, 'I', 0, 1, MIPSolver(:Cbc,LogLevel=0))
+    println("Solution status: ", solution.status)
+    println("Optimal value: ", solution.objval) # should be -7
+    print("Solution vector: ") 
+    show(solution.sol)
+    println()
 
 end
 
