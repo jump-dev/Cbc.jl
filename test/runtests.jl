a28cda6e2e49b1438b980268d7393edcdc264df9
Tony Kelman <tony@kelman.net>
2015-01-31 03:04:36 -0800

    use latest version 2.9.1
    should be fixed wrt clang and dependency linking
    
    run JuMP's tests here too

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,7 @@ using Cbc
 include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
 
 mixintprogtest(CbcSolver(logLevel=1))
+
+if isdir(Pkg.dir("JuMP"))
+    include(joinpath(Pkg.dir("JuMP"),"test","runtests.jl"))
+end
