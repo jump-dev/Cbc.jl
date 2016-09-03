75832e5b776c6165497251ee64ca67858eb6a95d
Miles Lubin <miles.lubin@gmail.com>
2016-09-02 18:23:45 -0600

    implement MathProgBase.setparameters

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,10 @@ include(joinpath(dirname(@__FILE__),"..","examples","knapsack.jl"))
 mixintprogtest(CbcSolver(logLevel=1))
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
-coniclineartest(CbcSolver())
+solver = CbcSolver(logLevel=1)
+MathProgBase.setparameters!(solver, Silent=true, TimeLimit=100.0)
+println("Cbc should not display any output from these tests")
+coniclineartest(solver)
 
 if isdir(Pkg.dir("JuMP"))
     include(joinpath(Pkg.dir("JuMP"),"test","runtests.jl"))
