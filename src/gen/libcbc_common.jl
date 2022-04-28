1760ce213d32bcbd0b01bd1ab23624e21868297d
odow <o.dowson@gmail.com>
2022-04-29 11:33:20 +1200

    Add license headers

--- src/gen/libcbc_common.jl
+++ src/gen/libcbc_common.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2013: Cbc.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 # Automatically generated using Clang.jl
 
 
